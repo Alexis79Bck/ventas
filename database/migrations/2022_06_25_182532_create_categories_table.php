3b5428439bffc0abeb7c92a548d968a74153cd91
Alexis Mata WebDev <alexedmata@gmail.com>
2022-06-25 17:43:09 -0400

    Añadido modelos, migraciones y controladores

--- database/migrations/2022_06_25_182532_create_categories_table.php
+++ database/migrations/2022_06_25_182532_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('fullname');
-            $table->string('phone');
-            $table->string('email')->unique();
-            $table->string('username')->unique();
-            $table->string('password');
-            $table->enum('status', ['ACTIVE', 'SUSPENDED', 'LOCKED'])->default('ACTIVE');
+            $table->string('name', 255);
             $table->string('image', 55)->nullable();
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -34,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 }
---
+++ database/migrations/2022_06_25_184909_create_sales_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedDecimal('total', 12, 2);
+            $table->integer('items');
+            $table->decimal('cash');
+            $table->decimal('change');
+            $table->enum('status', ['PAID', 'PENDING', 'CANCELED'])->default('PENDING');
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
