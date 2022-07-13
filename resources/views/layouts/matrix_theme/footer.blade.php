8a7b6d5bbae4ea9f0bb6e11ecdfe7c5223fa49e0
Alexis Mata WebDev <alexedmata@gmail.com>
2022-07-13 07:58:26 -0400

    Arreglos en Footers

--- resources/views/layouts/matrix_theme/footer.blade.php
+++ resources/views/layouts/matrix_theme/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="footer text-center">
-  Copyright © 2020. Sistema de Ventas. Designed and Developed by Alexis Mata.
+  Copyright © 2020. Sistema de Ventas. Developed by Alexis Mata.
   <p class="">version 1.0.0 </p>
 </footer>
